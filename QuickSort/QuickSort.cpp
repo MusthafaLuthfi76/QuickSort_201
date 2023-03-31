9a05874a983de75eea611d45a17d292148dd0aad
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-03-31 11:33:10 +0700

    int main

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -24,7 +24,7 @@ void input() {
 
     for (int i = 0; i < n; i++)
     {
-        cout << "<" << (i + 1) << ">":
+        cout << "<" << (i + 1) << ">";
         cin >> arr[i];
     }
 }
@@ -104,16 +104,11 @@ int main()
 {
     input();
     //Sort the array using quick sort
-    q - short(0, n - 1);
+    q_short(0, n - 1);
+    display();
+    system("pause");
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
