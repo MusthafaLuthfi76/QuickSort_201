6b4d4e59ecabf995c3c07607035ba8ed03c1a164
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-03-31 10:52:29 +0700

    Memasukkan values array

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -1,7 +1,11 @@
-// QuickSort.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+//Array of integers to hold values
+int arr[20];
+int cmp_count = 0;  // Number of comparison
+int mov_count = 0;  // Number of movement
+int n;
 
 int main()
 {
